96c13050dd3a01cdcc9eea5ee484ebc328aa7628
Sana Naik <53305879+sanasnaik@users.noreply.github.com>
2025-03-24 21:40:18 -0400

    Estimate position function complete (?)

--- p2/baseline_bot.py
+++ p2/baseline_bot.py
@@ -12,15 +12,60 @@ class Baseline:
                                     (1, 1), (1, -1), (-1, 1), (-1, -1)]
 
         self.estimated_pos = ()
-        self.possible_positions = self.spaceship.open_cells.copy()
 
     # Identify where the bot is
-    def get_est_pos(self):
-        
-        # Keep a knowledge base of open cells on the map
-        for key, item in self.possible_positions:
-            pass
+    def get_est_pos(self, actual_bot_pos):
+        self.possible_positions = self.spaceship.open_cells.copy()
+        num_neighbors = self.count_blocked_neighbors(actual_bot_pos)
 
+        # Keep a knowledge base of open cells on the map
+        for key, _ in self.possible_positions.items():
+
+            # Keep track of the number of currently blocked neighbors
+            self.possible_positions[key] = self.count_blocked_neighbors(key)
+
+            if self.possible_positions[key] != num_neighbors:  # if the bot's blocked cells don't match
+                self.possible_positions.pop(key)
+            
+        # Repeat until we get the bot's true position
+        curr = actual_bot_pos
+        while len(self.possible_positions) > 1:
+            # Keep track of the most commoonly open direction 
+            dir_frequency = {d: 0 for d in self.spaceship.neighbour_directions}
+
+            for pos in self.possible_positions:
+
+                for d in self.spaceship.neighbour_directions:
+                    new_pos = (pos[0] + d[0], pos[1] + d[1])
+
+                    if new_pos in self.spaceship.open_cells:
+                        dir_frequency[d] += 1
+        
+            # Choose the direction with the max count
+            most_open_dir = max(dir_frequency, key = dir_frequency.get)
+            # Attempt to move
+            new_bot_pos = (curr[0] + most_open_dir[0], curr[1] + most_open_dir[1])
+        
+            if new_bot_pos in self.spaceship.open_cells:
+                success = True
+                curr = new_bot_pos
+            else:
+                success = False
+            
+            # Update the other positions and attempt move
+            for pos in self.possible_positions:
+                pos_new = (pos[0] + most_open_dir[0], pos[1] + most_open_dir[1])
+                if success:  # if the move worked and the new pos did not
+                    if pos_new not in self.spaceship.open_cells:
+                        self.possible_positions.pop(pos)
+                else:
+                    # If the move failed, then positions that would have allowed the move should be removed
+                    if pos_new in self.spaceship.open_cells:
+                        self.possible_positions.pop(pos)
+        
+        # Once only one candidate remains, set it as the estimated position.
+        self.estimated_pos = self.possible_positions.pop()
+        return self.estimated_pos
 
     # returns True if ping is heard
     # sens is a constant specifying the sensitivity of the detector
@@ -49,13 +94,13 @@ class Baseline:
 
 
     # Sense how many of the neighboring eight cells are currently blocked
-    def count_blocked_neighbors(self):
+    def count_blocked_neighbors(self, cell):
         count = 0
         for dx, dy in self.eight_neighbor_dirs:
-            x = self.pos[0] + dx
-            y = self.pos[1] + dy
+            x = cell[0] + dx
+            y = cell[1] + dy
 
-            if 0 <= x <= 30 and 0 <= y <= 30 and self.spaceship[x][y] == 0:
+            if 0 <= x <= 30 and 0 <= y <= 30 and self.spaceship.grid[x][y] == 0:
                 count += 1
 
         return count
