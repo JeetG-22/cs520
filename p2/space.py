77c999d2da6ea0e06220ed090d6065ed834b629f
Sana Naik <53305879+sanasnaik@users.noreply.github.com>
2025-04-03 23:30:27 -0400

    Update space.py

--- p2/space.py
+++ p2/space.py
@@ -50,7 +50,7 @@ import numpy as np
 # Plot your results as a function of alpha.
 
 # 100 simulations with each alpha
-alpha = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+alpha = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
 N = 30
 num_of_sims = 100
@@ -89,4 +89,3 @@ plt.ylabel('Average Number of Moves to Catch Rat (100 trials)')
 plt.title('Average Number of Moves Taken to Catch Rat vs. Alpha')
 plt.legend()
 plt.show()
-plt.savefig("Plot2.png")
