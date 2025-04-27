661945e6b1f358d07f26c0a1d9d363065683c00f
Sana Naik <53305879+sanasnaik@users.noreply.github.com>
2025-04-27 10:42:35 -0400

    Working on bot 1

--- p3/space.py
+++ p3/space.py
@@ -1,6 +1,10 @@
 import ship
+import bot
 
 # Note: ship does not have closed cells bordering its edges for some reason
-ship = ship.Ship(6)
-ship.place_entities()
-print(ship)
+s = ship.Ship(6)
+s.place_entities()
+print(s)
+
+b = bot.Bot(s)
+print(b.get_est_pos())
