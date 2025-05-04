c9c3f9a85cac8a447cd727f804c6cd214cab3b40
Sana Naik <53305879+sanasnaik@users.noreply.github.com>
2025-05-04 12:15:42 -0400

    fdasfda

--- p3/bot.py
+++ p3/bot.py
@@ -8,7 +8,6 @@ class Bot:
     def __init__(self, ship: ship.Ship):
         self.spaceship = ship
         self.actual_bot_pos = self.get_position(2)
-        self.L = {k: k for k in self.spaceship.open_cells}  # hashtable storing the open cells initial position and current position
 
     # Attempt to move in a direction
     def get_new_pos(self, bot_pos, dir):
@@ -22,20 +21,16 @@ class Bot:
     
 
     # Implement the solution in part 2
-    def get_moves(self):  # L is the set of possible locations
+    def get_moves(self, L = None):  # L is the set of possible locations
         
-        # Get an open cell that is a dead end or corner
-        # Get a list of dead end cells - cells that don't have neighbors on opposite sides
-        dead_end_cells = self.L.copy()
-        for cell in dead_end_cells.copy():
-            x = cell[0]
-            y = cell[1]
-            if x - 1 >= 0 and x + 1 < self.spaceship.N and self.spaceship.grid[x-1][y] == 1 and self.spaceship.grid[x+1][y] == 1:
-                dead_end_cells.pop(cell)
-            elif y - 1 >= 0 and y + 1 < self.spaceship.N and self.spaceship.grid[x][y-1] == 1 and self.spaceship.grid[x][y+1] == 1:
-                dead_end_cells.pop(cell)
+        if not L:
+            self.L = {k: k for k in self.spaceship.open_cells}  # hashtable storing the open cells initial position and current position
+        else:
+            self.L = L
+            if len(L) == 1:
+                return self.L.popitem()[0], 0
 
-        target, _ = random.choice(list(dead_end_cells.items()))
+        target = self.get_dead_end_cell(self.L)
 
         # store the total sequence of moves
         all_moves = []
@@ -52,19 +47,40 @@ class Bot:
 
             while i < len(path) and len(self.L) > 1:
                 move = path[i]
-                all_moves.append(move)
                 dir = (move[0] - curr[0], move[1] - curr[1])
                 self.L, success = self.get_L_next(self.L[self.actual_bot_pos], self.L, dir)
                 curr = move
+                all_moves.append(curr)
                 i += 1
-                if success != True:  # bot wasn't able to continue down this path
+                if not success:  # bot wasn't able to continue down this path
                     break
             
             # Reset L's values
             self.L = {k: k for k, _ in self.L.items()}
         
-        return self.L.popitem()[0]
+        return self.L.popitem()[0], len(all_moves)
 
+    # Returns random open dead end cell
+    def get_dead_end_cell(self, L = None):
+        # Get an open cell that is a dead end or corner
+        # Get a list of dead end cells - cells that don't have neighbors on opposite sides
+        if not L:
+            self.dead_L = {k: k for k in self.spaceship.open_cells}
+        else:
+            self.dead_L = L
+    
+        dead_end_cells = {k: k for k in self.dead_L}
+
+        for cell in dead_end_cells.copy():
+            x = cell[0]
+            y = cell[1]
+            if (x-1, y) in self.spaceship.open_cells and (x+1, y) in self.spaceship.open_cells:
+                dead_end_cells.pop(cell)
+            elif (x, y-1) in self.spaceship.open_cells and (x, y+1) in self.spaceship.open_cells:
+                dead_end_cells.pop(cell)
+
+        target, _ = random.choice(list(dead_end_cells.items()))
+        return target
 
     def get_path(self, source, target):
 
--- p3/space.py
+++ p3/space.py
@@ -1,10 +1,42 @@
 import ship
 import bot
+import random
 
 # Note: ship does not have closed cells bordering its edges for some reason
-s = ship.Ship(5)
+s = ship.Ship(10)
 s.place_entities()
 print(s)
 
 b = bot.Bot(s)
 print(b.get_moves())
+
+target = b.get_dead_end_cell()
+
+# List of possible locations with bot pos in the beginning
+L_values = [b.actual_bot_pos, target] + [cell for cell, _ in s.open_cells.items() if cell != b.actual_bot_pos and cell != target] 
+print(L_values)
+
+for i in range(1, len(L_values) + 1):
+    L_list = L_values[:i]
+    L_table = {k: k for k in L_list}
+    print(L_table)
+    pos, num_moves = b.get_moves(L_table)
+    print(num_moves)
+
+num_moves_per_L = []
+
+# for _ in range(100):
+#     s = ship.Ship(20)
+#     s.place_entities()
+#     b = bot.Bot(s)
+
+#     # List of possible locations with bot pos in the beginning
+#     L_values = [b.actual_bot_pos] + [cell for cell, _ in s.open_cells.items() if cell != b.actual_bot_pos] 
+
+#     for i in range(1, len(L_values) + 1):
+#         L_list = L_values[:i]
+#         L_table = {k: k for k in L_list}
+#         pos, num_moves = b.get_moves(L_table)
+#         num_moves_per_L.append(num_moves)
+
+# print(num_moves_per_L)
